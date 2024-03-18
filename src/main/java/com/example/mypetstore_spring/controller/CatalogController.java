dea1551dd8607132ea69f8f45b8dcb2194988317
zs <1041824212@qq.com>
2024-03-18 15:13:37 +0800

    thymeleaf

---
+++ src/main/java/com/example/mypetstore_spring/controller/CatalogController.java
@@ -0,0 +1,14 @@
+package com.example.mypetstore_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+    @GetMapping("/view")
+    public String view(){
+        return "catalog/main";
+    }
+}
